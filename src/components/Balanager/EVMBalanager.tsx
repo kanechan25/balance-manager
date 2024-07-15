b3bd3e7a4ade0d76cde8595eb0224a15458fd1d9
kanechan25 <kt.itengineer@gmail.com>
2024-07-15 18:34:29 +0700

    refactor code

--- src/components/Balanager/EVMBalanager.tsx
+++ src/components/Balanager/EVMBalanager.tsx
@@ -20,41 +20,46 @@ const EVMBalanager = () => {
 
     return (
       <div style={{ border: `2px solid ${borderColor}` }} className={`rounded-xl p-4 w-full`} key={chainData.chainId}>
-        {chainData?.dataInChain?.map((accountBl, index) => {
-          return (
-            <div key={accountBl?.accountAddress} className={`${index !== 0 && `border-t border-dashed `} flex w-full gap-1 relative py-2`}>
-              <div className="w-28 flex gap-2">
-                <img src={chains[chainId].logo} alt="" className="w-6 h-6" />
-                <div>{chains[chainId].displayName}</div>
-              </div>
-              <LightTooltip title="Click to Copy!" followCursor>
-                <div onClick={() => handleCopyArr(accountBl?.accountAddress)} className="w-52 h-fit cursor-pointer">
-                  {shortenAddress(accountBl?.accountAddress, 10, 10)}
+        <div className="w-full overflow-x-scroll">
+          {chainData?.dataInChain?.map((accountBl, index) => {
+            return (
+              <div
+                key={accountBl?.accountAddress}
+                className={`${index !== 0 && `border-t border-dashed `} flex w-full gap-1 relative py-2`}
+              >
+                <div className="w-28 min-w-28 flex gap-2">
+                  <img src={chains[chainId].logo} alt="" className="w-6 h-6" />
+                  <div>{chains[chainId].displayName}</div>
                 </div>
-              </LightTooltip>
-              <div className="total-market-value flex flex-col justify-between min-w-32">
-                <div>Total Market</div>
-                <div className="text-green-500 text-lg font-bold">${formatTokenNumDecimal(accountBl?.value?.marketValue, 4)}</div>
+                <LightTooltip title="Click to Copy!" followCursor>
+                  <div onClick={() => handleCopyArr(accountBl?.accountAddress)} className="w-52 min-w-52 h-fit cursor-pointer">
+                    {shortenAddress(accountBl?.accountAddress, 10, 10)}
+                  </div>
+                </LightTooltip>
+                <div className="total-market-value flex flex-col justify-between w-32 min-w-32">
+                  <div>Total Market</div>
+                  <div className="text-green-500 text-lg font-bold">${formatTokenNumDecimal(accountBl?.value?.marketValue, 4)}</div>
+                </div>
+                <div className="native-token w-36 min-w-36 flex flex-col">
+                  <div>{accountBl?.nativeBalance?.symbol}</div>
+                  <div>{formatTokenNumDecimal(accountBl?.nativeBalance?.balance, 8)}</div>
+                  <div className="text-green-500">${formatTokenNumDecimal(accountBl?.nativeBalance?.value?.marketValue, 4)}</div>
+                </div>
+                <div className="erc20-token flex gap-2">
+                  {accountBl?.tokenBalances?.map((token: any) => {
+                    return (
+                      <div className="w-44 min-w-44" key={token?.address}>
+                        <div>{token?.symbol}</div>
+                        <div>{formatTokenNumDecimal(token?.balance, 8)}</div>
+                        <div className="text-green-500">${formatTokenNumDecimal(token?.value?.marketValue, 4)}</div>
+                      </div>
+                    )
+                  })}
+                </div>
               </div>
-              <div className="native-token min-w-36 flex flex-col">
-                <div>{accountBl?.nativeBalance?.symbol}</div>
-                <div>{formatTokenNumDecimal(accountBl?.nativeBalance?.balance, 8)}</div>
-                <div className="text-green-500">${formatTokenNumDecimal(accountBl?.nativeBalance?.value?.marketValue, 4)}</div>
-              </div>
-              <div className="erc20-token flex gap-2">
-                {accountBl?.tokenBalances?.map((token: any) => {
-                  return (
-                    <div className="min-w-44" key={token?.address}>
-                      <div>{token?.symbol}</div>
-                      <div>{formatTokenNumDecimal(token?.balance, 8)}</div>
-                      <div className="text-green-500">${formatTokenNumDecimal(token?.value?.marketValue, 4)}</div>
-                    </div>
-                  )
-                })}
-              </div>
-            </div>
-          )
-        })}
+            )
+          })}
+        </div>
       </div>
     )
   })
--- src/components/Balanager/SOLBalanager.tsx
+++ src/components/Balanager/SOLBalanager.tsx
@@ -17,36 +17,38 @@ const SOLBalanager = () => {
 
   return (
     <div style={{ border: `2px solid ${borderColor}` }} className={`rounded-xl p-4`}>
-      {transformSOLData?.map((chainData, index) => {
-        return (
-          <div key={chainData.addrSol} className={`${index !== 0 && `border-t border-dashed `} flex w-full gap-1 relative py-2`}>
-            <div className="w-28  flex gap-2">
-              <img src={chains[0].logo} alt="" className="w-6 h-6" />
-              <div>{chains[0].displayName}</div>
-            </div>
-            <LightTooltip title="Click to Copy!" followCursor>
-              <div onClick={() => handleCopyArr(chainData?.addrSol)} className="w-52 h-fit cursor-pointer">
-                {shortenAddress(chainData?.addrSol, 10, 10)}
+      <div className="w-full overflow-x-scroll">
+        {transformSOLData?.map((chainData, index) => {
+          return (
+            <div key={chainData.addrSol} className={`${index !== 0 && `border-t border-dashed `} flex w-full gap-1 relative py-2`}>
+              <div className="w-28 min-w-28 flex gap-2">
+                <img src={chains[0].logo} alt="" className="w-6 h-6" />
+                <div>{chains[0].displayName}</div>
               </div>
-            </LightTooltip>
-            <div className="total-market-value flex flex-col justify-between min-w-32">
-              <div>Total Market</div>
-              <div className="text-green-500 text-lg font-bold">${formatTokenNumDecimal(chainData?.dataInChain?.value?.total, 4)}</div>
+              <LightTooltip title="Click to Copy!" followCursor>
+                <div onClick={() => handleCopyArr(chainData?.addrSol)} className="w-52 min-w-52 h-fit cursor-pointer">
+                  {shortenAddress(chainData?.addrSol, 10, 10)}
+                </div>
+              </LightTooltip>
+              <div className="total-market-value flex flex-col justify-between w-32 min-w-32">
+                <div>Total Market</div>
+                <div className="text-green-500 text-lg font-bold">${formatTokenNumDecimal(chainData?.dataInChain?.value?.total, 4)}</div>
+              </div>
+              <div className="spl-token flex gap-2">
+                {chainData?.dataInChain?.tokens.map((token: any, index: number) => {
+                  return (
+                    <div className="min-w-44 w-44" key={token?.name}>
+                      <div>{token?.symbol}</div>
+                      <div>{formatTokenNumDecimal(token?.balance, 8)}</div>
+                      <div className="text-green-500">${formatTokenNumDecimal(token?.value?.marketValue, 4)}</div>
+                    </div>
+                  )
+                })}
+              </div>
             </div>
-            <div className="spl-token flex gap-2">
-              {chainData?.dataInChain?.tokens.map((token: any, index: number) => {
-                return (
-                  <div className="min-w-44" key={token?.name}>
-                    <div>{token?.symbol}</div>
-                    <div>{formatTokenNumDecimal(token?.balance, 8)}</div>
-                    <div className="text-green-500">${formatTokenNumDecimal(token?.value?.marketValue, 4)}</div>
-                  </div>
-                )
-              })}
-            </div>
-          </div>
-        )
-      })}
+          )
+        })}
+      </div>
     </div>
   )
 }
